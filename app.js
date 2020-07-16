7d6cde3c0f717993da2c9dbbc321f0383f5f0d16
TFicici <tuba1918@hotmail.com>
2020-07-16 11:08:08 +0300

    control added

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ app.get("/generateURLControl/:url",(req,res)=>{
   var alinanURL=req.params.url;
   
   conn.collection('url').findOne({key:alinanURL},function(err,key){
-      res.send('OK');
+      res.send(key);
   });
   
 });
